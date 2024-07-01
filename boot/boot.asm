713ce7f2e190c2ad732cbabf938d904b9773fea4
pratush <pratushrai0309@gmail.com>
2024-07-01 22:41:26 +0530

    disk_sect_disk can read sectors from the disk

--- boot/boot.asm
+++ boot/boot.asm
@@ -3,8 +3,8 @@ mov bp, 0x8000
 mov sp, bp
 
 mov bx, 0x9000
-mov dh, 2
-call disk_load
+mov dh, 3 ; No. of sectors to read
+call disk_load ; loads the data read from disk (0x9000)
 mov dx, [0x9000]
 call print_hex
 
@@ -12,6 +12,13 @@ call print_nl
 
 mov dx, [0x9000 + 512]
 call print_hex
+
+call print_nl
+
+mov dx, [0x9000 + 2*512]
+call print_hex
+
+call print_nl
 
 jmp $
 
@@ -24,4 +31,5 @@ times 510-($-$$) db 0
 db 0x55, 0xaa
 
 times 256 dw 0xdada
-times 256 dw 0xface
+times 256 dw 0xfafd
+times 256 dw 0xaa00
--- boot/boot_sect_disk.asm
+++ boot/boot_sect_disk.asm
@@ -2,14 +2,14 @@ disk_load:
     pusha
     push dx
 
-    mov ah, 0x02
-    mov al, dh
-    mov cl, 0x02
+    mov ah, 0x02 ; Set function code to 0x02 (read sectors) 
+    mov al, dh 
+    mov cl, 0x02 ; Read Second Sector(first sector after boot sector)
 
-    mov ch, 0x00
-    mov dh, 0x00
+    mov ch, 0x00 ; Cylinder number 0
+    mov dh, 0x00 ; Head No. 0
 
-    int 0x13
+    int 0x13 ; loads the data from disk to (0x9000) [if error carry bit is set]
     jc disk_error
 
     pop dx
